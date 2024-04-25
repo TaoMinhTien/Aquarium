80931594e9573433c854a193ad57bd84a0895937
minhquang <quang.nm.2391@aptechlearning.edu.vn>
2024-04-25 20:18:39 +0700

    siderbar

--- resources/views/layout/siderbar.blade.php
+++ resources/views/layout/siderbar.blade.php
@@ -1,46 +1,34 @@
-
 <div class="wrapper_quang">
    <nav id="sidebar_quang">
-      <div class="sidebar-header_quang">
-         <h3></h3>
-      </div>
       <ul class="list-unstyled components_quang">
-         <li class="active_quang">
-            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle_quang">Home</a>
-            <ul class="collapse list-unstyled_li" id="homeSubmenu">
+         <li>
+            <a href="#">Home</a>
+         </li>
+         <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle_quang">Animals</a>
+         <ul class="collapse list-unstyled_li" id="homeSubmenu">
+            <li>
+               <a href="">Information</a>
+            </li>
+            <li>
+               <a href="">Habitat</a>
+            </li>
+         </ul>
+         <div>
+            <a href="#eventSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle_quang">Tickets</a>
+            <ul class="collapse list-unstyled_li" id="eventSubmenu">
                <li>
-                  <a href="#">Home 1</a>
+                  <a href="">Upload</a>
                </li>
                <li>
-                  <a href="#">Home 2</a>
-               </li>
-               <li>
-                  <a href="#">Home 3</a>
+                  <a href="">Edit</a>
                </li>
             </ul>
+         </div>
+         <li>
+            <a href="">Portfolio</a>
          </li>
          <li>
-            <a href="#">About</a>
-         </li>
-         <li>
-            <a href="#" class="dropdown-toggle_quang" onclick="toggleSubMenu('pageSubmenu')">Pages</a>
-            <ul class="collapse list-unstyled_li" id="pageSubmenu">
-               <li>
-                  <a href="#">Page 1</a>
-               </li>
-               <li>
-                  <a href="#">Page 2</a>
-               </li>
-               <li>
-                  <a href="#">Page 3</a>
-               </li>
-            </ul>
-         </li>
-         <li>
-            <a href="#">Portfolio</a>
-         </li>
-         <li>
-            <a href="#">Contact</a>
+            <a href="">Contact</a>
          </li>
       </ul>
    </nav>
