cf9afdcc74e07423c82275787511b5a1f2c08fa8
minhquang <quang.nm.2391@aptechlearning.edu.vn>
2024-04-28 14:15:20 +0700

    s
    s

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\CheckoutController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
@@ -8,17 +10,6 @@ use App\Http\Controllers\NewsController;
 use App\Http\Controllers\ReadController;
 use App\Http\Controllers\TicketController;
 use App\Http\Controllers\ContactController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('layout/Homepage');
@@ -30,15 +21,18 @@ Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'HandleRegister'])->name('register.edit');
 Route::get('/logout', [LoginController::class, 'HandleLogout'])->name('logout');
 Route::get('/news', [NewsController::class, 'news'])->name('news.view');
+Route::get('/checkout', [CheckoutController::class, 'checkout'])->name('checkout.view');
+Route::get('/cart', [CartController::class, 'cartView'])->name('cart.view');
+Route::get('/tickets/view', [TicketController::class, 'ticketview'])->name('ticket.view');
+Route::get('/tickets', [TicketController::class, 'ticket'])->name('ticket');
+Route::get('/contact', [ContactController::class, 'contactView'])->name('contact.view');
 Route::post('/news/read/', [ReadController::class, 'newsRead'])->name('news.read');
-Route::Post('/tickets', [TicketController::class, 'BuyTicket'])->name('tickets.buy');
+Route::Post('/tickets/buy', [TicketController::class, 'BuyTicket'])->name('tickets.buy');
 Route::Post('/handle/contact', [ContactController::class, 'HandleContact'])->name('handle.contact');
-
-
-
-
 // Các route của trang admin viết ở trong đây//////
 Route::middleware('auth.admin')->group(function () {
     Route::get('/admin', [DashboardController::class, 'index'])->name('admin.dashboard');
+    Route::get('/news/upload', [NewsController::class, 'uploadNews'])->name('news.upload');
+    Route::post('/news/handle/upload', [NewsController::class, 'handleUploadNews'])->name('handle.upload.news');
 });
 ////////////
