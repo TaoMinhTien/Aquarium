d55e35ca45ada1f45f39ebb629cfb8132e6f2f04
HieuLeTrung1501 <hieu.lt.2394@aptechlearning.edu.vn>
2024-05-02 18:57:37 +0700

    fix navbar

---
+++ database/hieu.sql
@@ -0,0 +1,127 @@
+-- CREATE TABLE 'Banner'(
+--     'id' INT NOT NULL,
+--     'title' VARCHAR(50) NOT NULL,
+--     'description' VARCHAR(200) NOT NULL
+-- )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- CREATE TABLE 'Banner_box'(
+--     'id' INT NOT NULL,
+--     'title' VARCHAR(50) NOT NULL,
+--     'description' VARCHAR(200) NOT NULL
+-- )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- CREATE TABLE `Banner_images` (
+--   `id` bigint(20) UNSIGNED NOT NULL,
+--   `Banner_id` int(11) DEFAULT NULL,
+--   `image_url` varchar(255) NOT NULL
+-- )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- CREATE TABLE `Banner_box_images` (
+--   `id` bigint(20) UNSIGNED NOT NULL,
+--   `Banner_box_id` int(11) DEFAULT NULL,
+--   `image_url` varchar(255) NOT NULL
+-- )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- CREATE TABLE 'Information' (
+--     'id' INT NOT NULL,
+--     'title' VARCHAR(50) NOT NULL,
+--     'description' VARCHAR(200) NOT NULL
+-- )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- CREATE TABLE 'Information_images' (
+--   `id` bigint(20) UNSIGNED NOT NULL,
+--   `Information_id` int(11) DEFAULT NULL,
+--   `image_url` varchar(255) NOT NULL
+-- )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- CREATE TABLE 'Overview'(
+--     'id' INT NOT NULL,
+--     'title' VARCHAR(50) NOT NULL,
+--     'description' VARCHAR(200) NOT NULL
+-- )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- CREATE TABLE 'Overview_images' (
+--   `id` bigint(20) UNSIGNED NOT NULL,
+--   `Overview_id` int(11) DEFAULT NULL,
+--   `image_url` varchar(255) NOT NULL
+-- )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- CREATE TABLE 'Gallery'(
+--     'id' INT NOT NULL,
+--     'description' VARCHAR(100) NOT NULL
+-- )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- CREATE TABLE 'Gallery_images' (
+--   `id` bigint(20) UNSIGNED NOT NULL,
+--   `Gallery_id` int(11) DEFAULT NULL,
+--   `image_url` varchar(255) NOT NULL
+-- )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- CREATE TABLE 'AboutUs'(
+--     'id' INT NOT NULL,
+--     'client_name' VARCHAR(50) NOT NULL,
+--     'description' VARCHAR(200) NOT NULL
+-- )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- CREATE TABLE 'AboutUs_images' (
+--   `id` bigint(20) UNSIGNED NOT NULL,
+--   `AboutUs_id` int(11) DEFAULT NULL,
+--   `image_url` varchar(255) NOT NULL
+-- )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- ALTER TABLE `Banner`
+--   ADD PRIMARY KEY (`id`);
+
+-- ALTER TABLE `Banner_box`
+--   ADD PRIMARY KEY (`id`);
+
+-- ALTER TABLE `Banner_images`
+--   ADD PRIMARY KEY (`id`),
+--   ADD FOREIGN KEY (`Banner_id`) REFERENCES `Banner` (`id`);
+
+-- ALTER TABLE `Banner_box_images`
+--   ADD PRIMARY KEY (`id`),
+--   ADD FOREIGN KEY (`Banner_box_id`) REFERENCES `Banner_box` (`id`);
+
+-- ALTER TABLE `Information`
+--   ADD PRIMARY KEY (`id`);
+
+-- ALTER TABLE `Information_images`
+--   ADD PRIMARY KEY (`id`),
+--   ADD FOREIGN KEY (`Information_id`) REFERENCES `Information` (`id`);
+
+-- ALTER TABLE `Overview`
+--   ADD PRIMARY KEY (`id`);
+
+-- ALTER TABLE `Overview_images`
+--   ADD PRIMARY KEY (`id`),
+--   ADD FOREIGN KEY (`Overview_id`) REFERENCES `Overview` (`id`);
+
+-- ALTER TABLE `Gallery`
+--   ADD PRIMARY KEY (`id`);
+
+-- ALTER TABLE `Gallery_images`
+--   ADD PRIMARY KEY (`id`),
+--   ADD FOREIGN KEY (`Gallery_id`) REFERENCES `Gallery` (`id`);
+
+-- ALTER TABLE `AboutUs`
+--   ADD PRIMARY KEY (`id`);
+
+-- ALTER TABLE `AboutUs_images`
+--   ADD PRIMARY KEY (`id`),
+--   ADD FOREIGN KEY (`AboutUs_id`) REFERENCES `AboutUs` (`id`);
+
+
+
