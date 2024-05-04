c9fcfb5de38d44f28db154997cee939a96eedae5
Thanhbi2612 <157878943+Thanhbi2612@users.noreply.github.com>
2024-05-04 19:29:20 +0700

    fbf

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,7 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ReadController;
 use App\Http\Controllers\TicketController;
-use App\Http\Controllers\NewsController;
-use App\Http\Controllers\ContactController;
-<<<<<<< HEAD
+use App\Http\Controllers\NewsController;use App\Http\Controllers\ContactController;
 use App\Http\Controllers\AnimalsController;
 use App\Http\Controllers\CoralReefController;
 use App\Http\Controllers\DolphinController;
@@ -23,12 +21,7 @@ use App\Http\Controllers\OpenOceanController;
 use App\Http\Controllers\SeaTurtleController;
 use App\Http\Controllers\SharkController;
 use App\Http\Controllers\Guide1Controller;
-use App\Http\Controllers\Guide2Controller;
-use App\Http\Controllers\UserController;
-use App\Models\User;    
-=======
-use App\Http\Controllers\FeedbackController;
->>>>>>> 7e38fca07d97491cc791e61f7f83b0e8328cf0a2
+use App\Http\Controllers\Guide2Controller;use App\Http\Controllers\UserController;use App\Models\User;    
 
 Route::get('/', function () {
     return view('layout/Homepage');
