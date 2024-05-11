a17e385c8ed81c20ec9ade955933167e40d5dc55
TienAdonis <tienadonis97@gmail.com>
2024-05-11 09:03:58 +0700

    commit

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -25,32 +25,10 @@ class ContactController extends Controller
             'description' => 'required',
             'gender' => 'required',
         ]);
-        // if ($request->filled('gender')) {
-        //     // Nếu trường gender được gửi lên
-
-        //     // Kiểm tra giá trị của gender
-        //     if ($request->gender === 'on') {
-        //         // Nếu giá trị của gender là 'on', thì đặt giá trị thành 'Male'
-        //         $validatedData['gender'] = 'Male';
-        //     } else {
-        //         // Nếu giá trị của gender không phải là 'on', thì giữ nguyên giá trị
-        //         $validatedData['gender'] = $request->gender;
-        //     }
-
-        //     // Kiểm tra xem giá trị của gender có phải là 'Male' hoặc 'Female' không
-        //     if (!in_array($validatedData['gender'], ['Male', 'Female'])) {
-        //         return response()->json(['message' => 'Invalid gender value'], 422);
-        //     }
-        // }
         if ($request->filled('gender')) {
-            // Nếu trường gender được gửi lên
-
-            // Kiểm tra giá trị của gender
             if ($request->gender === 'Male' || $request->gender === 'Female') {
-                // Nếu giá trị của gender là 'Male' hoặc 'Female', thì giữ nguyên giá trị
                 $validatedData['gender'] = $request->gender;
             } else {
-                // Nếu giá trị của gender không phải là 'Male' hoặc 'Female', trả về lỗi
                 return response()->json(['message' => 'Invalid gender value'], 422);
             }
         }
